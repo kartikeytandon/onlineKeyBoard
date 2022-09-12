6c797d2fa4935dd7e785ea94ba079b2d7197a134
Kartikey Tandon <91780794+kartikeytandon@users.noreply.github.com>
2022-09-12 17:22:07 +0530

    index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 let caps = 0
 
 function Cap() {
-    if(caps === 0) {
-        caps = 1 
+    if (caps === 0) {
+        caps = 1
         console.log("caps on")
         document.getElementById("capsLock").style.backgroundColor = "#29465B"
     } else {
@@ -11,223 +11,255 @@ function Cap() {
         console.log("caps off")
     }
 }
+
 function callBackspace() {
     let str = text.innerText
-    text.innerText = str.substring(0, (str.length-1))
+    text.innerText = str.substring(0, (str.length - 1))
 }
+
 function spacebar() {
     console.log("I was clicked")
     let a = '&nbsp'
     document.getElementById("text").innerHTML += a
 }
+
 function enter() {
-    console.log("I was clicked")
+    // console.log("I was clicked")
     document.getElementById("text").innerHTML += "<br>"
 }
 
 function callItQ() {
-    if(caps === 1) {
+    if (caps === 1) {
         document.getElementById("text").innerText += document.getElementById("box1").innerText
     } else {
-        document.getElementById("text").innerText    += document.getElementById("box1").innerText.toLowerCase() 
+        document.getElementById("text").innerText += document.getElementById("box1").innerText.toLowerCase()
     }
 }
+
 function callItW() {
-    if(caps === 1) {
+    if (caps === 1) {
         document.getElementById("text").innerText += document.getElementById("box2").innerText
     } else {
-        document.getElementById("text").innerText += document.getElementById("box2").innerText.toLowerCase() 
+        document.getElementById("text").innerText += document.getElementById("box2").innerText.toLowerCase()
     }
 }
+
 function callItE() {
-    if(caps === 1) {
+    if (caps === 1) {
         document.getElementById("text").innerText += document.getElementById("box3").innerText
     } else {
-        document.getElementById("text").innerText += document.getElementById("box3").innerText.toLowerCase() 
+        document.getElementById("text").innerText += document.getElementById("box3").innerText.toLowerCase()
     }
 }
+
 function callItR() {
-    if(caps === 1) {
+    if (caps === 1) {
         document.getElementById("text").innerText += document.getElementById("box4").innerText
     } else {
-        document.getElementById("text").innerText += document.getElementById("box4").innerText.toLowerCase() 
+        document.getElementById("text").innerText += document.getElementById("box4").innerText.toLowerCase()
     }
 }
+
 function callItT() {
-    if(caps === 1) {
+    if (caps === 1) {
         document.getElementById("text").innerText += document.getElementById("box5").innerText
     } else {
-        document.getElementById("text").innerText += document.getElementById("box5").innerText.toLowerCase() 
+        document.getElementById("text").innerText += document.getElementById("box5").innerText.toLowerCase()
     }
 }
+
 function callItY() {
-    if(caps === 1) {
+    if (caps === 1) {
         document.getElementById("text").innerText += document.getElementById("box6").innerText
     } else {
-        document.getElementById("text").innerText += document.getElementById("box6").innerText.toLowerCase() 
+        document.getElementById("text").innerText += document.getElementById("box6").innerText.toLowerCase()
     }
 }
+
 function callItU() {
-    if(caps === 1) {
+    if (caps === 1) {
         document.getElementById("text").innerText += document.getElementById("box7").innerText
     } else {
-        document.getElementById("text").innerText += document.getElementById("box7").innerText.toLowerCase() 
+        document.getElementById("text").innerText += document.getElementById("box7").innerText.toLowerCase()
     }
 }
+
 function callItI() {
-    if(caps === 1) {
+    if (caps === 1) {
         document.getElementById("text").innerText += document.getElementById("box8").innerText
     } else {
-        document.getElementById("text").innerText += document.getElementById("box8").innerText.toLowerCase() 
+        document.getElementById("text").innerText += document.getElementById("box8").innerText.toLowerCase()
     }
 }
+
 function callItO() {
-    if(caps === 1) {
+    if (caps === 1) {
         document.getElementById("text").innerText += document.getElementById("box9").innerText
     } else {
-        document.getElementById("text").innerText += document.getElementById("box9").innerText.toLowerCase() 
+        document.getElementById("text").innerText += document.getElementById("box9").innerText.toLowerCase()
     }
 }
+
 function callItP() {
-    if(caps === 1) {
+    if (caps === 1) {
         document.getElementById("text").innerText += document.getElementById("box10").innerText
     } else {
-        document.getElementById("text").innerText += document.getElementById("box10").innerText.toLowerCase() 
+        document.getElementById("text").innerText += document.getElementById("box10").innerText.toLowerCase()
     }
 }
+
 function callItA() {
-    if(caps === 1) {
+    if (caps === 1) {
         document.getElementById("text").innerText += document.getElementById("box11").innerText
     } else {
-        document.getElementById("text").innerText += document.getElementById("box11").innerText.toLowerCase() 
+        document.getElementById("text").innerText += document.getElementById("box11").innerText.toLowerCase()
     }
 }
+
 function callItS() {
-    if(caps === 1) {
+    if (caps === 1) {
         document.getElementById("text").innerText += document.getElementById("box12").innerText
     } else {
-        document.getElementById("text").innerText += document.getElementById("box12").innerText.toLowerCase() 
+        document.getElementById("text").innerText += document.getElementById("box12").innerText.toLowerCase()
     }
 }
+
 function callItD() {
-    if(caps === 1) {
+    if (caps === 1) {
         document.getElementById("text").innerText += document.getElementById("box13").innerText
     } else {
-        document.getElementById("text").innerText += document.getElementById("box13").innerText.toLowerCase() 
+        document.getElementById("text").innerText += document.getElementById("box13").innerText.toLowerCase()
     }
 }
+
 function callItF() {
-    if(caps === 1) {
+    if (caps === 1) {
         document.getElementById("text").innerText += document.getElementById("box14").innerText
     } else {
-        document.getElementById("text").innerText += document.getElementById("box14").innerText.toLowerCase() 
+        document.getElementById("text").innerText += document.getElementById("box14").innerText.toLowerCase()
     }
 }
+
 function callItG() {
-    if(caps === 1) {
+    if (caps === 1) {
         document.getElementById("text").innerText += document.getElementById("box15").innerText
     } else {
-        document.getElementById("text").innerText += document.getElementById("box15").innerText.toLowerCase() 
+        document.getElementById("text").innerText += document.getElementById("box15").innerText.toLowerCase()
     }
 }
+
 function callItH() {
-    if(caps === 1) {
+    if (caps === 1) {
         document.getElementById("text").innerText += document.getElementById("box16").innerText
     } else {
-        document.getElementById("text").innerText += document.getElementById("box16").innerText.toLowerCase() 
+        document.getElementById("text").innerText += document.getElementById("box16").innerText.toLowerCase()
     }
 }
+
 function callItJ() {
-    if(caps === 1) {
+    if (caps === 1) {
         document.getElementById("text").innerText += document.getElementById("box17").innerText
     } else {
-        document.getElementById("text").innerText += document.getElementById("box17").innerText.toLowerCase() 
+        document.getElementById("text").innerText += document.getElementById("box17").innerText.toLowerCase()
     }
 }
+
 function callItK() {
-    if(caps === 1) {
+    if (caps === 1) {
         document.getElementById("text").innerText += document.getElementById("box18").innerText
     } else {
-        document.getElementById("text").innerText += document.getElementById("box18").innerText.toLowerCase() 
+        document.getElementById("text").innerText += document.getElementById("box18").innerText.toLowerCase()
     }
 }
+
 function callItL() {
-    if(caps === 1) {
+    if (caps === 1) {
         document.getElementById("text").innerText += document.getElementById("box19").innerText
     } else {
-        document.getElementById("text").innerText += document.getElementById("box19").innerText.toLowerCase() 
+        document.getElementById("text").innerText += document.getElementById("box19").innerText.toLowerCase()
     }
 }
+
 function callItZ() {
-    if(caps === 1) {
+    if (caps === 1) {
         document.getElementById("text").innerText += document.getElementById("box20").innerText
     } else {
-        document.getElementById("text").innerText += document.getElementById("box20").innerText.toLowerCase() 
+        document.getElementById("text").innerText += document.getElementById("box20").innerText.toLowerCase()
     }
 }
+
 function callItX() {
-    if(caps === 1) {
+    if (caps === 1) {
         document.getElementById("text").innerText += document.getElementById("box21").innerText
     } else {
-        document.getElementById("text").innerText += document.getElementById("box21").innerText.toLowerCase() 
+        document.getElementById("text").innerText += document.getElementById("box21").innerText.toLowerCase()
     }
 }
+
 function callItC() {
-    if(caps === 1) {
+    if (caps === 1) {
         document.getElementById("text").innerText += document.getElementById("box22").innerText
     } else {
-        document.getElementById("text").innerText += document.getElementById("box22").innerText.toLowerCase() 
+        document.getElementById("text").innerText += document.getElementById("box22").innerText.toLowerCase()
     }
 }
+
 function callItV() {
-    if(caps === 1) {
+    if (caps === 1) {
         document.getElementById("text").innerText += document.getElementById("box23").innerText
     } else {
-        document.getElementById("text").innerText += document.getElementById("box23").innerText.toLowerCase() 
+        document.getElementById("text").innerText += document.getElementById("box23").innerText.toLowerCase()
     }
 }
+
 function callItB() {
-    if(caps === 1) {
+    if (caps === 1) {
         document.getElementById("text").innerText += document.getElementById("box24").innerText
     } else {
-        document.getElementById("text").innerText += document.getElementById("box24").innerText.toLowerCase() 
+        document.getElementById("text").innerText += document.getElementById("box24").innerText.toLowerCase()
     }
 }
+
 function callItN() {
-    if(caps === 1) {
+    if (caps === 1) {
         document.getElementById("text").innerText += document.getElementById("box25").innerText
     } else {
-        document.getElementById("text").innerText += document.getElementById("box25").innerText.toLowerCase() 
+        document.getElementById("text").innerText += document.getElementById("box25").innerText.toLowerCase()
     }
 }
+
 function callItM() {
-    if(caps === 1) {
+    if (caps === 1) {
         document.getElementById("text").innerText += document.getElementById("box26").innerText
     } else {
-        document.getElementById("text").innerText += document.getElementById("box26").innerText.toLowerCase() 
+        document.getElementById("text").innerText += document.getElementById("box26").innerText.toLowerCase()
     }
 }
+
 function specialChar1() {
-    if(caps === 1) {
+    if (caps === 1) {
         document.getElementById("text").innerText += ","
     } else {
         document.getElementById("text").innerText += "!"
     }
 }
+
 function specialChar2() {
-    if(caps === 1) {
+    if (caps === 1) {
         document.getElementById("text").innerText += "?"
     } else {
         document.getElementById("text").innerText += "."
     }
 }
+
 function specialChar3() {
-    if(caps === 1) {
+    if (caps === 1) {
         document.getElementById("text").innerText += "^"
     } else {
         document.getElementById("text").innerText += "`"
     }
 }
+
 function nums() {
     document.getElementById("box1").innerText = 1
     document.getElementById("box2").innerText = 2
@@ -257,11 +289,13 @@ function nums() {
     document.getElementById("box26").innerText = "^"
     document.getElementById("box27").innerText = "! ,"
     document.getElementById("box28").innerText = ". ?"
-    // document.getElementById("numm").innerText = "@gmail.com"
+        // document.getElementById("numm").innerText = "@gmail.com"
 }
+
 function print() {
     document.getElementById("text").innerText += "@gmail.com"
 }
+
 function normalState() {
     document.getElementById("box1").innerText = "Q"
     document.getElementById("box2").innerText = "W"
@@ -292,4 +326,27 @@ function normalState() {
 }
 document.addEventListener("keypress", event => {
     document.getElementById("text").innerText += event.key
-}) 
+})
+document.addEventListener("keydown", event => {
+        if (event.key === "Backspace") {
+            callBackspace()
+        }
+    })
+    // document.addEventListener("keydown", event => {
+    //     if (event.key == "Enter") {
+    //         enter()
+    //     }
+    // })
+document.body.onkeyup = function(e) {
+    if (e.key == " " ||
+        e.code == "Space" ||
+        e.keyCode == 32
+    ) {
+        spacebar()
+    }
+}
+document.addEventListener("keyup", event => {
+    if (event.key == "Enter") {
+        enter()
+    }
+})
